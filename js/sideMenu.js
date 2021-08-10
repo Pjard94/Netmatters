1a668a5422f666a881872b57cdc984e63a7e10fd
Peter <jardine.peter.94@gmail.com>
2021-08-10 16:57:37 +0100

    Adding side menu, aswell as style and script

---
+++ js/sideMenu.js
@@ -0,0 +1,6 @@
+const smallHamburger = document.getElementById('hamburger--sm')
+const hamburger = document.getElementById('hamburger--more')
+
+smallHamburger.addEventListener('click', function () {
+    
+})
