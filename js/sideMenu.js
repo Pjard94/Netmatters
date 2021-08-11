5e35a538db3ecf8c8e350feec5179b0141f40f3c
Peter <jardine.peter.94@gmail.com>
2021-08-11 16:45:22 +0100

    Finished the functionality of the side menu

--- js/sideMenu.js
+++ js/sideMenu.js
@@ -1,6 +1,25 @@
-const smallHamburger = document.getElementById('hamburger--sm')
-const hamburger = document.getElementById('hamburger--more')
+const smallHamburger = document.getElementById('hamburger--sm');
+const hamburger = document.getElementById('hamburger--more');
+const body = document.body;
+const page = document.getElementById('menu-overlay')
+let binary = 0;
+
+page.addEventListener('click', function (){
+    body.classList.remove('menu-showing')
+    body.classList.remove('large-menu-showing')
+    binary = 0;
+})
 
 smallHamburger.addEventListener('click', function () {
-    
+    if (binary === 0) {
+       body.classList.add('menu-showing'); 
+       binary = 1;
+    }
+})
+
+hamburger.addEventListener('click', function () {
+    if (binary === 0) {
+       body.classList.add('menu-showing'); 
+       binary = 1;
+    }
 })
