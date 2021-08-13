a88e3525c238ef40e8fc9e02b8e63212db3c0604
Peter <jardine.peter.94@gmail.com>
2021-08-13 16:38:42 +0100

    Added parts to the slider fixing issues I had.

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+$('.slides').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+});
