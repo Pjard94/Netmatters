1658e966d765d5113c9ac471620e3722e4e56327
Peter <jardine.peter.94@gmail.com>
2021-08-20 17:01:08 +0100

    Added a local storage check

--- js/cookie.js
+++ js/cookie.js
@@ -1,11 +1,19 @@
 const accept = document.getElementById('cookie-accept-button');
-const cookieModal = document.getElementById("cookie-container")
+const cookieModal = document.getElementById("cookie-container");
 
-window.addEventListener('load', function () {
-    cookieModal.classList.add('cookie-check');
+window.addEventListener('load', function() {
+    const cookieCheck = localStorage.getItem('PeterCookie');
+    if (cookieCheck === null) {
+        cookieModal.classList.add('cookie-check');
+    } else {
+        cookieModal.classList.remove('cookie-check');
+        document.getElementById('cookie-container').style.display = "none";
+    }
+        
 })
 
 accept.addEventListener('click', function(){
+    localStorage.setItem('PeterCookie', 1)
     cookieModal.classList.remove('cookie-check');
     document.getElementById('cookie-container').style.display = "none";
 })
