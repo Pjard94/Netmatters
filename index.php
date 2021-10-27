de5a6854f5d82fd2843a3e575d98df2d345e86f1
Peter <jardine.peter.94@gmail.com>
2021-10-27 15:38:17 +0100

    Add my footer to the correct file  and linked it

--- index.php
+++ index.php
@@ -364,59 +364,8 @@
             <!-- <span class="fill"><i class="far fa-square "></i></span> <i class="fas fa-check-square"></i>-->
             </div>
 
-            <footer class="footer">
+            <?php include("inc/footer.php"); ?>
 
-                <div class="row--f">
-
-                    <!--The contact us section of the footer  -->
-                    <div class="contact-area column">
-                        <h3 class="footer-heading">Contact us</h3>
-                        <ul>
-                            <li>11 Penfold Drive</li>
-                            <li>Wymondham</li>
-                            <li>Norfolk</li>
-                            <li>NR18 0WZ</li>
-                        </ul>
-                        <p>Tel:<a href="">01603 70 40 20</a></p>
-                        <p>Email: <a href="mailto:#" target="_blank">support@netmatters.com</a></p>
-                    </div>
-
-                    <!--The links to about us pages section of the footer  -->
-                    <div class="about-area column">
-                        <h3 class="footer-heading"> About Netmatters</h3>
-                        <ul>
-                            <li><a href="#" rel="The latest publications">News</a></li>
-                            <li><a href="#" rel="Careers oppotunities we have.">Our Careers</a></li>
-                            <li><a href="#" rel="Our team members">Our Team</a></li>
-                            <li><a href="#" rel="A virtual tour of our office">Our Office Tour</a></li>
-                            <li><a href="#" rel="How we will keep your information safe.">Privacy Policy</a></li>
-                            <li><a href="#" rel="The details of our cookies.">Cookie Policy</a></li>
-                            <li><a href="#" rel="Our terms and conditions page.">Terms & Conditions</a></li>
-                            <li><a href="#" rel="Everything you need for domains.">UK Domains</a></li>
-                        </ul>
-                    </div>
-
-                    <!--The site map section of the footer  -->
-                    <div class="site-map column">
-                        <h3 class="footer-heading">Website</h3>
-                        <p><a href="" target="_blank" rel="The map of the Netmatters site.">Sitemap</a></p>
-                        <p>&copy; Copyright Netmatters Ltd. 2021</p>
-                        <p>All rights reserved</p>
-                    </div>
-
-                    <!--The social media links section of the footer  -->
-                    <div class="Sm-buttons column">
-                        <h3 class="footer-heading">Social Media</h3>
-                        <a href="#" class="fb social-media-buttons" rel="The companies Facebook page." target="_blank"><i class="fab fa-facebook-f"></i></a>
-                        <a href="" class="tw social-media-buttons" rel="The companies Twitter profile." target="_blank"><i class="fab fa-twitter"></i></a>
-                        <a href="" class="sta social-media-buttons" rel="The companies instagram page. in" target="_blank"><i class="fab fa-instagram"></i></a>
-                        <a href="" class="in social-media-buttons" rel="The companies LinkedIn page. in" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-
-                </div>
-
-            </footer>
-            
                 <!-- Awards section -->
             <div class="awards">
                 <div class="awards--container">
