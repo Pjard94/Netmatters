5116d9f1ec371dd7368bb47fd8c926ed7f797a50
Peter <jardine.peter.94@gmail.com>
2021-10-27 15:40:00 +0100

    added jquery link that was missing

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- This helps with internet explorer-->
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> <!--This helps to set up mobile viewing-->
         <title>Netmatters</title>
+        <!-- allows the jQuery stuff to run -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <!-- Stylesheet links -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="sass/slick.css"/>
         <link rel="stylesheet" href="sass/slick-theme.css">
