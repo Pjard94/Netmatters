4ec6b2be6f1080c3ec29c5e38d5911a509be5ecb
Peter <jardine.peter.94@gmail.com>
2021-10-28 17:00:10 +0100

    Added more to the contact page

--- contact.php
+++ contact.php
@@ -25,22 +25,86 @@
             <div id="contact-container">
 
                 <div id="contact-banner">
-                    <h3><a>Home</a></h3>
-                    <ul class="banner-list">
-                        <li>Our Offices</li>
-                    </ul>
+                    <div class="inner">
+                        <a href="#"><h3>Home</h3></a>
+                        <ul class="banner-list">
+                            <li>Our Offices</li>
+                        </ul>
+                    </div>
+                    
                 </div>
 
                 <div id="contact-title">
-                    <h1>Our Offices</h1>
+                    <div class="inner">
+                        <h1 id="office-page-title">Our Offices</h1>
+                    </div>
                 </div>
 
                 <div id="office-addresses">
-                    <div class="office-cards"></div>
-                    <div class="map-container"></div>
+                    <div id="office-list-container">
+                        <div class="office-columns">
+                            <div class="office-cards">
+                                <img src="Assets/cambridge.jpg" alt="" class="office-images">
+                                <div class="office-card-text">
+                                    <a href="#">Cambridge Office</a>
+                                    <p>Unit 1.28,<br>
+                                    St John's Innovation Centre,<br>
+                                    Cowley Road, Milton,<br>
+                                    Cambridge,<br>
+                                    CB4 0WS</p>
+                                    <a href="#" class="text-design">01223 37 57 72</a>
+                                    <a href="#" class="design-button office-button">view more</a>
+                                </div>
+                            </div>
+                            <div class="map-container">
+
+                            </div>
+                        </div>
+                        <div class="office-columns">
+                            <div class="office-cards">
+                                <img src="Assets/wymondham.jpg" alt="" class="office-images">
+                                <div class="office-card-text">
+                                    <a href="#">Wymondham Office</a>
+                                    <p>Unit 15,<br>
+                                    Penfold Drive,<br>
+                                    Gateway 11 Business Park,<br>
+                                    Wymondham, Norfolk,<br>
+                                    NR18 0WZ</p>
+                                    <a href="#" class="text-design">01603 70 40 20</a>
+                                    <a href="#" class="design-button office-button">view more</a>
+                                </div>
+                            </div>
+                            <div class="map-container">
+
+                            </div>
+                        </div>
+                        <div class="office-columns">
+                            <div class="office-cards">
+                                <img src="Assets/yarmouth-2.jpg" alt="" class="office-images">
+                                <div class="office-card-text">
+                                    <a href="#">Great Yarmouth Office</a>
+                                    <p>Suite F23,<br>
+                                    Beacon Innovation Centre,<br>
+                                    Beacon Park, Gorleston,<br>
+                                    Great Yarmouth, Norfolk,<br>
+                                    NR31 7RA</p>
+                                    <a href="#" class="text-design">01493 60 32 04</a>
+                                    <a href="#" class="design-button office-button">view more</a>
+                                </div>
+                            </div>
+                            <div class="map-container">
+
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
 
-                <div id="contact-area"></div>
+                <div id="contact-area">
+                    <div class="inner">
+
+                    </div>
+                </div>
 
             </div>
 
