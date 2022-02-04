b561d61fd8f6b1f076afd11ab21746b2e4cd38ba
ChaosMiner65 <82098806+Pjard94@users.noreply.github.com>
2022-02-04 08:39:42 +0000

    Update databaseCall.php

--- inc/databaseCall.php
+++ inc/databaseCall.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $PDO = new PDO("mysql:host=localhost;dbname=peterjar_netmatters", "peterjar_contact_admin", "zEoH^W90O=yp");
+    $PDO = new PDO("mysql:host=localhost;dbname=contact", "contact_admin", "zEoH^W90O=yp");
 } catch (PDOException $e) {
     // echo $e->getMessage();
 }
